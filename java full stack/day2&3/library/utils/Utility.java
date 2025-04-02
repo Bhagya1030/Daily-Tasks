8946e19ab4127eec5ec45707a74a5595740fe7b7
BhagyaLakshmi Akkannagari <133506987+Bhagya1030@users.noreply.github.com>
2025-04-02 11:49:29 +0530

    Delete java full stack directory

--- java full stack/day2&3/library/utils/Utility.java
+++
@@ -1,8 +0,0 @@
-package day2.library.utils;
-import java.util.UUID;
-
-public class Utility {
-    public static String generateId() {
-        return UUID.randomUUID().toString().substring(0, 8); // Generates a short unique ID
-    }
-}
